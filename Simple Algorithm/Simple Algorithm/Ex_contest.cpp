4c389625b84d00af244f249319d4fbdd1d0a9b4e
danek0100 <71225718+danek0100@users.noreply.github.com>
2021-01-08 11:49:00 +0300

    + end 5 lecture + some ex

--- Simple Algorithm/Simple Algorithm/Ex_contest.cpp
+++ Simple Algorithm/Simple Algorithm/Ex_contest.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
 
 
+
+//Наиьбольшее чётное число в последовательности
 int main()
 {
 	int x;
 	std::cin >> x;
-	int count = 0;
+	int max = 0;
 	while (x != 0)
 	{
-		if (x % 2 == 0) count++;
+		if (x % 2 == 0 && x > max) max = x;
 		std::cin >> x;
 	}
-	std::cout << count;
+	std::cout << max;
 	return 0;
 }
+
+
+//Точная степень двойки
+//int main()
+//{
+//	int N;
+//	std::cin >> N;
+//	int two_pow = 1;
+//	while (two_pow < N)
+//	{
+//		two_pow *= 2;
+//	}
+//	if (two_pow == N)
+//		std::cout << "YES";
+//	else
+//		std::cout << "NO";
+//	return 0;
+//}
+
+//Количество чётных чисел
+//int main()
+//{
+//	int x;
+//	std::cin >> x;
+//	int count = 0;
+//	while (x != 0)
+//	{
+//		if (x % 2 == 0) count++;
+//		std::cin >> x;
+//	}
+//	std::cout << count;
+//	return 0;
+//}
 
 //Високосный год
 //int main()
--- Simple Algorithm/Simple Algorithm/Mass and sort.cpp
+++ Simple Algorithm/Simple Algorithm/Mass and sort.cpp
@@ -33,5 +33,87 @@ using namespace std;
 //		A[top++] = x;
 //		cin >> x;
 //	}
+//	while (top > 0)
+//		std::cout << A[--top] << '\t';
+//	cout << '\n';
+//	return 0;
+//}
+
+//chek of sort
+//int main()
+//{
+//	int const N = 5;
+//	int A[N] = { 1, 2, 3, 4, 5 };
+//	
+//	bool is_sorted = true;
+//
+//	int i = 0;
+//	while (i < N - 1) {
+//		if (A[i] > A[i + 1])
+//		{
+//			is_sorted = false;
+//			break;
+//		}
+//		i++;
+//	}
+//
+//	cout << is_sorted;
+//	return 0;
+//}
+
+//stupid_sort
+//int main()
+//{
+//	int const N = 5;
+//	int A[N] = { 1, 3, 2, 4, 5 };
+//	int i = 0;
+//
+//	while (i < N - 1) {
+//		if (A[i] > A[i + 1])
+//		{
+//			int temp = A[i];
+//			A[i] = A[i + 1];
+//			A[i + 1] = temp;
+//			i = 0;
+//		}
+//		else i++;
+//	}
+//
+//	int n = N;
+//	while (n > 0)
+//		std::cout << A[--n] << '\t';
+//	cout << '\n';
+//	return 0;
+//}
+
+// Существует ассимтотическая оценка сложности алгоритма N = k * f(x) или O(N)
+//Существует объезьяния сортировка, когда мы трясём ящик (выбираем случайные перестановк
+//и проверяем на отсортированнность такой алгорити имеет сложность O(n!)
+
+//bouble sort O(n^2)
+//int main()
+//{
+//	int const N = 5;
+//	int A[N] = { 1, 3, 2, 4, 5 };
+//	bool is_sorted = false;
+//	while (!is_sorted) {
+//		int i = 0;
+//		is_sorted = true;
+//		while (i < N - 1) {
+//			if (A[i] > A[i + 1])
+//			{
+//				int temp = A[i];
+//				A[i] = A[i + 1];
+//				A[i + 1] = temp;
+//				is_sorted = false;
+//			}
+//			i++;
+//		}
+//	}
+//
+//	int n = N;
+//	while (n > 0)
+//		std::cout << A[--n] << '\t';
+//	cout << '\n';
 //	return 0;
 //}
