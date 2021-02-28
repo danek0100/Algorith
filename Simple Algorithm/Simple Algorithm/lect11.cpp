c7f93b65d7415d235b700b4a6364505473157ecd
danek0100 <71225718+danek0100@users.noreply.github.com>
2021-02-28 10:25:08 +0300

    end of 11 lect

--- Simple Algorithm/Simple Algorithm/lect11.cpp
+++ Simple Algorithm/Simple Algorithm/lect11.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
 
 //Очень быстро растёт дерево Фибоначи, чем больше n тем больше дерево
 //O(Fi(n)) - сложность равна числу фибоначи, количество вызвов для предыдущих
@@ -32,4 +35,44 @@ uint64_t fib_dynamic(int n) {
 //	for (int n = 0; n < 60; n++) {
 //		std::cout << n << '\t' << fib_recursive(n) << '\t' << fib_dynamic(n) << '\n';
 //	}
+//}
+
+//Кузнечик (динамическое программирование)
+//Можно удалить массив, но тогда не получится найти обратный путь
+uint64_t min_cost(int n, uint64_t* price) {
+	uint64_t* cost = new uint64_t[n+1];
+	cost[1] = price[1];
+	cost[2] = price[1] + price[2];
+	uint64_t result = 0;
+
+	for (int i = 3; i <= n; i++) {
+		cost[i] = std::min(cost[i - 1], cost[i - 2]) + price[i];
+	}
+
+
+	int current = n;
+	std::cout << "Min path (reversed)" << '\n';
+	std::cout << current << " ";
+	while (current != 1) {
+		if (cost[current - 1] == cost[current] - price[current])
+			current = current - 1;
+		else if (cost[current - 2] == cost[current] - price[current])
+		current = current - 2;
+		else 
+			throw -1;
+		std::cout << current << " ";
+	}
+	result = cost[n];
+	return result;
+}
+
+//int main() {
+//	int n;
+//	std::cin >> n;
+//	uint64_t* price = new uint64_t[n + 1];
+//	for (int i = 1; i <= n; n++) {
+//		std::cin >> price[i];
+//	}
+//	std::cout << "Min cost = " << min_cost(n, price) << '\n';
+//	delete[] price;
 //}
