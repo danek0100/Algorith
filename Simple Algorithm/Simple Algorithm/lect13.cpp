7da498eba6415a4fec31bc9d086cf7a9b588c2b2
danek0100 <71225718+danek0100@users.noreply.github.com>
2021-04-26 19:30:40 +0300

    something

--- Simple Algorithm/Simple Algorithm/lect13.cpp
+++ Simple Algorithm/Simple Algorithm/lect13.cpp
@@ -262,4 +262,20 @@ std::vector<int> prefix_function_kmp(std::string s) {
 // [a-z]*[0-9]? - сколько-то букв, а в конце одно число
 
 // Хеш функции
-// 
+// Алгоритм Рабина_Карпа
+
+#include <iostream>
+#include <string>
+
+
+//const int P = 257;
+//
+//uint32_t hash(std::string s) {
+//	uint32_t sum = 0;
+//	uint32_t factor = 1;
+//	 
+//	for (int i = s.length() - 1; i >= 0; i--) {
+//		sum += s[i] * factor;
+//		factor *= P;
+//	}
+//}
